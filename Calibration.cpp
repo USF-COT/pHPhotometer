2e007ba068728fbe68712da0c6c24a12c50a490f
Michael Lindemuth <lindemuth@gmail.com>
2014-06-11 10:43:22 -0400

    Testing added calibration code

---
+++ Calibration.cpp
@@ -0,0 +1,47 @@
+#include "Calibration.h"
+#include <math.h>
+#include <SdFat.h>
+
+Calibration::Calibration(){
+}
+
+Calibration::~Calibration(){
+}
+
+const unsigned int LINE_SIZE = 64;
+boolean Calibration::load(){
+  Serial.println("Hello!");
+  return true;
+  /*
+  char filename[16];
+  strncpy(filename, this->name, 4);
+  strncat(filename, "CAL.CSV", 6);
+  Serial.println(filename);
+  
+  char buffer[LINE_SIZE];
+  ifstream sdin(filename);
+  if(!sdin.is_open()){
+    Serial.println("Unable to open calibration file");
+    this->order = 0;
+    return false;
+  }
+  
+  char comma;
+  sdin >> this->order;
+  for(byte i=0; i <= this->order; ++i){
+    sdin >> comma;
+    sdin >> this->parameters[i];
+  }
+  
+  return true;
+  */
+}
+
+float Calibration::adjustReading(float reading){
+  float calibratedValue = 0;
+  for(byte i=0; i <= this->order; ++i){
+    calibratedValue += this->parameters[i] * pow(reading, this->order-i);
+  }
+  return calibratedValue;
+}
+
---
+++ Calibration.h
@@ -0,0 +1,19 @@
+#ifndef CALIBRATION_H
+#define CALIBRATION_H
+
+#include <Arduino.h>
+
+class Calibration{
+  private:
+    String name;
+    
+    byte order;
+    float parameters[4];  // 3rd order calibration limit
+  public:
+    Calibration();
+    ~Calibration();
+    boolean load();
+    float adjustReading(float value);
+};
+
+#endif
